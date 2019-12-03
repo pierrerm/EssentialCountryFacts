bf7c6ffbb3be0283323be6348b9d4eb22155cbd5
Glitch (hello-webpage) <none>
2019-12-03 19:55:01 +0000

    💝🌒 Checkpoint ./script.js:8556467/989 ./style.css:8556467/3136 ./index.html:8556467/8071

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+var positionButton = document.getElementById("positionButton");
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+function getISSData() {
+  var jsonObj = $.getJSON(
+    "https://api.wheretheiss.at/v1/satellites/25544",
+    updatePosition
+  );
+}
+
+function updatePosition(jsonObj) {
+  var gmap_canvas = document.getElementById("gmap_canvas");
+  var latitude = jsonObj.latitude;
+  var longitude = jsonObj.longitude;
+  gmap_canvas.src = "https://www.arcgis.com/home/webscene/viewer.html?viewpoint=cam:" + latitude + "," + longitude + ",35000000;0,0&ui=min";
+  document.getElementById("issPicture").style.display = "inline-block";
+}
+
+function findISS() {
+  getISSData();
+}
+
+$(document).ready(function(){
+    $("iframe").load(function(){
+        $(this).contents().on("mousedown, mouseup, click", function(){
+            alert("Click detected inside iframe.");
+        });
+    });
+});
