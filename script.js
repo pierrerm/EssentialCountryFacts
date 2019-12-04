2aeb202803aaa4d7feef825aa143fe1ccb0f0f0b
Glitch (hello-webpage) <none>
2019-12-04 01:43:44 +0000

    🚇🌶 Checkpoint ./index.html:8556467/63 ./script.js:8556467/919

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 var positionButton = document.getElementById("positionButton");
 
-function getISSData() {
+function searchLocation(){
+  var location = document.getElementById("location");
+  var value = "https://www.metaweather.com/api/location/search/?query=" + location.value;
+  console.log(value);
   var jsonObj = $.getJSON(
-    "https://api.wheretheiss.at/v1/satellites/25544",
-    updatePosition
+    value,
+    updateSuggestions
   );
 }
 
-function updatePosition(jsonObj) {
-  var gmap_canvas = document.getElementById("gmap_canvas");
-  var latitude = jsonObj.latitude;
-  var longitude = jsonObj.longitude;
-  gmap_canvas.src = "https://www.arcgis.com/home/webscene/viewer.html?viewpoint=cam:" + latitude + "," + longitude + ",35000000;0,0&ui=min";
-  document.getElementById("issPicture").style.display = "inline-block";
-}
-
-function findISS() {
-  getISSData();
+function updateSuggestions(jsonObj) {
+  var city = jsonObj[0].title;
+  console.log(city);
+  // var gmap_canvas = document.getElementById("gmap_canvas");
+  // var latitude = jsonObj.latitude;
+  // var longitude = jsonObj.longitude;
+  // gmap_canvas.src = "https://www.arcgis.com/home/webscene/viewer.html?viewpoint=cam:" + latitude + "," + longitude + ",35000000;0,0&ui=min";
+  // document.getElementById("issPicture").style.display = "inline-block";
 }
 
 $(document).ready(function(){
