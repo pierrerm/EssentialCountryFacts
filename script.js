ea114285772aef3346419cade10682e0ccba7015
Glitch (hello-webpage) <none>
2019-12-04 23:23:40 +0000

    🍪💴 Checkpoint ./index.html:8556467/285 ./style.css:8556467/107 ./script.js:8556467/4669

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var positionButton = document.getElementById("positionButton");
 
-function searchLocation(){
+function searchLocation() {
   var location = document.getElementById("location");
   var value = location.value;
   console.log(value);
@@ -11,33 +11,46 @@ function searchLocation(){
 }
 
 function updateSuggestions(jsonObj) {
-  var city = jsonObj[0].capital;
-  console.log(city);
-  document.getElementById("countryName").innerHTML = jsonObj[0].name;
-  document.getElementById("flag").src = jsonObj[0].flag;
-  document.getElementById("continent").innerHTML = "Continent: " + jsonObj[0].subregion;
-  document.getElementById("capital").innerHTML = "Capital: " + jsonObj[0].capital;
-  document.getElementById("population").innerHTML = "Population: " + jsonObj[0].population;
-  document.getElementById("demonym").innerHTML = "Demonym: " + jsonObj[0].demonym;
-  document.getElementById("language").innerHTML = "Language: " + jsonObj[0].languages[0].name;
-  document.getElementById("currency").innerHTML = "Currency: " + jsonObj[0].currencies[0].code + ", "+ jsonObj[0].currencies[0].name;
-  var countryInfo = document.getElementById("countryInfo");
-  countryInfo.style = "display: inline-block;";
-  // var gmap_canvas = document.getElementById("gmap_canvas");
-  // var latitude = jsonObj.latitude;
-  // var longitude = jsonObj.longitude;
-  // gmap_canvas.src = "https://www.arcgis.com/home/webscene/viewer.html?viewpoint=cam:" + latitude + "," + longitude + ",35000000;0,0&ui=min";
-  // document.getElementById("issPicture").style.display = "inline-block";
+  console.log(jsonObj);
+  if (jsonObj[0].message == undefined) {
+    var city = jsonObj[0].capital;
+    console.log(city);
+    document.getElementById("countryName").innerHTML = jsonObj[0].name;
+    document.getElementById("flag").src = jsonObj[0].flag;
+    document.getElementById("continent").innerHTML =
+      "Region: " + jsonObj[0].subregion;
+    document.getElementById("capital").innerHTML =
+      "Capital: " + jsonObj[0].capital;
+    document.getElementById("population").innerHTML =
+      "Population: " + jsonObj[0].population;
+    document.getElementById("demonym").innerHTML =
+      "Demonym: " + jsonObj[0].demonym;
+    document.getElementById("language").innerHTML =
+      "Language: " + jsonObj[0].languages[0].name;
+    document.getElementById("currency").innerHTML =
+      "Currency: " +
+      jsonObj[0].currencies[0].code +
+      ", " +
+      jsonObj[0].currencies[0].name;
+    var countryInfo = document.getElementById("countryInfo");
+    countryInfo.style = "display: inline-block;";
+  } else {
+    var value = document.getElementById("location").value;
+    document.getElementById("errorMessage").innerHTML = "Error! The country '" + value + "' could not be found...";
+    document.getElementById("ErrorBox").style = "display: inline-block;";
+  }
 }
 
-function getWeather(){
+function getWeather() {
   searchLocation();
 }
 
-$(document).ready(function(){
-    $("iframe").load(function(){
-        $(this).contents().on("mousedown, mouseup, click", function(){
-            alert("Click detected inside iframe.");
-        });
-    });
+$(document).ready(function() {
+  $("iframe").load(function() {
+    $(this)
+      .contents()
+      .on("mousedown, mouseup, click", function() {
+        alert("Click detected inside iframe.");
+      });
+  });
 });
